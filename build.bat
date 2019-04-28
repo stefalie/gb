08fd7d3fe3c1215cbe1a3a7f36e72671b4b79810
Stefan Lienhard <sl@stefanlienhard.ch>
2019-04-28 10:42:35 +0200

    added full path in diagnostic text fo vs code

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 
 set SdlDir=..\external\SDL2-2.0.9
 set SdlLibs=%SdlDir%\lib\x64\SDL2.lib %SdlDir%\lib\x64\SDL2main.lib
@@ -7,6 +7,6 @@ set CodeFiles=..\code\gb.c
 mkdir build
 pushd build
 copy "%SdlDir%\lib\x64\SDL2.dll" .
-cl /Zi /I%SdlDir%\include %CodeFiles% %SdlLibs% /link /SUBSYSTEM:console
+cl /FC /Zi /I%SdlDir%\include %CodeFiles% %SdlLibs% /link /SUBSYSTEM:console
 popd
 
